3ec925071ebb3a0f13ebad90f9e5526a06dfd083
zakariye <zakariye1942@icloud.com>
2024-04-08 19:27:17 +0200

    categories

--- client/components/categories.js
+++ client/components/categories.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, } from 'react-native'
+import { useState } from 'react';
 import React from 'react'
+import { categories } from '../constants';
 
 export default function Categories() {
+    const {activeCategory , setActiveCategory } = useState ('null');
   return (
-    <View>
-      <Text>Categories</Text>
+    <View className = "mt-4">
+      <ScrollView horizontal 
+      ShowHorizontalScrollIndicator = {false} 
+      className ='overflow-visible' 
+      contentContainerStyle = {{paddingHorizontal : 15}} >
+   {  categories.map((category, index) => {
+  return (
+    <View key={index} className="flex justify-center items-center mr-6">
+      <TouchableOpacity
+        onPress={() => {}}
+        className="p-1 rounded-full shadow bg-gray-200">
+        <Image style={{ width: 45, height: 45 }} source={category.image} />
+        <Text className="text-sm">{category.name}</Text>
+      </TouchableOpacity>
+    </View>
+  )
+})
+}
+
+
+      </ScrollView>
     </View>
   )
 }
